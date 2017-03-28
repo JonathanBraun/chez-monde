fb2a4d17f086d3040369c7861be5129606688194
Jonathan Braun <Jonathanrbraun@gmail.com>
2017-03-28 12:17:32 -0400

    add background

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,56 @@
 
 worlds_best = {
   "2017" = {
+    Osteria Francescana (Modena, Italy),
+    El Celler de Can Roca (Girona, Spain),
+    Eleven Madison Park (New York City, U.S.A),
+    Central (Lima, Peru),
+    Noma (Copenhagen, Denmark),
+    Mirazur (Menton, France),
+    Mugaritz (Errenteria, Spain),
+    Narisawa (Tokyo, Japan),
+    Steirereck (Vienna, Austria),
+    Asador Etxebarri (Axpe, Spain),
+    D.O.M. (São Paulo, Brazil),
+    Quintonil (Mexico City, Mexico),
+    Maido (Lima, Peru),
+    The Ledbury (London, UK),
+    Alinea (Chicago, U.S.A),
+    Azurmendi (Larrabetzu, Spain),
+    Piazza Duomo (Alba, Italy),
+    White Rabbit (Moscow, Russia),
+    L'Arpege (Paris, France),
+    Amber (Hong Kong),
+    Arzak (San Sebastian, Spain),
+    Test Kitchen (Cape Town, South Africa),
+    Gaggan (Bangkok, Thailand),
+    Le Bernardin (New York City, U.S.A),
+    Pujol (Mexico City, Mexico),
+    The Clove Club (London, UK),
+    Saison (San Francisco, U.S.A),
+    Geranium (Copenhagen, Denmark),
+    Tickets (Barcelona, Spain),
+    Astrid y Gaston (Lima, Peru),
+    RyuGin (Tokyo, Japan),
+    Restaurant Andre (Singapore),
+    Attica (Melbourne, Australia),
+    Restaurant Tim Raue (Berlin, Germany),
+    Vendôme (Bergisch Gladbach, Germany),
+    Boragó (Santiago, Chile),
+    Nahm (Bangkok, Thailand),
+    De Librije (Zwolle, the Netherlands),
+    Le Calandre (Rubano, Italy),
+    Relae (Copenhagen, Denmark),
+    Fäviken (Järpen, Sweden),
+    Ultraviolet (Shanghai, China),
+    Biko (Mexico City),
+    Estela (New York City, U.S.A),
+    Dinner by Heston Blumenthal (London, UK),
+    Combal Zero (Rivoli, Italy),
+    Schloss Schauenstein (Fürstenau, Switzerland),
+    Blue Hill at Stone Barns (Pocantico Hills, New York, U.S.A),
+    QuiQue Dacosta (Denia, Spain),
+    Septime (Paris, France),
 
   },
 
@@ -16,6 +66,6 @@ worlds_best = {
   },
 
   "2015" = {
-    
+
   }
 }
